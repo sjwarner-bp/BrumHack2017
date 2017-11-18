9306cae4ebf20aaa5111a825892177541de2eaf2
Nik Mouzourides <nikolas.mouzourides@blackpepper.co.uk>
2017-11-18 21:57:57 +0000

    Cleaned up NoteBank array

--- src/InstrumentPlayer.java
+++ src/InstrumentPlayer.java
@@ -6,26 +6,13 @@ class InstrumentPlayer{
     private static MidiChannel[] channels;
     private static Integer pitch;
     private static Integer volume;
-    private static String[] NoteBank;
+    private static String[] noteBank;
     private static int[] sharpNotes = {1, 3, 6, 8, 10};
     private static boolean isSound = false;
 
     InstrumentPlayer(){
         isSound = true;
-        NoteBank = new String[12];
-        NoteBank[0] = "C";
-        NoteBank[1] = "C#";
-        NoteBank[2] = "D";
-        NoteBank[3] = "D#";
-        NoteBank[4] = "E";
-        NoteBank[5] = "F";
-        NoteBank[6] = "F#";
-        NoteBank[7] = "G";
-        NoteBank[8] = "G#";
-        NoteBank[9] = "A";
-        NoteBank[10] = "A#";
-        NoteBank[11] = "B";
-
+        noteBank = new String[]{"C","C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
         Synthesizer synthesizer = null;
         try {
             synthesizer = MidiSystem.getSynthesizer();
@@ -86,7 +73,7 @@ class InstrumentPlayer{
         channels[0].noteOn((pitch), (volume));
         System.out.println("Volume: " + volume);
         System.out.println("Pitch: " + pitch);
-        System.out.println(NoteBank[NoteOut]);
+        System.out.println(noteBank[NoteOut]);
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {

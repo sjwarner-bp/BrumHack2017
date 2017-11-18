be479dc742190af58ded204da8cc15ea025ef777
Nik Mouzourides <nikolas.mouzourides@blackpepper.co.uk>
2017-11-18 18:39:10 +0000

    Changed volume and pitch to change using a set method instead of increase and decrease

--- src/InstrumentPlayer.java
+++ src/InstrumentPlayer.java
@@ -6,37 +6,50 @@ import javax.sound.midi.Synthesizer;
 class InstrumentPlayer{
     private static Instrument[] instruments;
     private static Integer pitch = 48;
-    private static Integer volume = 80;
+    private static Integer volume = 100;
     private static boolean isSound = false;
 
-    public static void increasePitch(){
-        pitch++;
-        if (pitch > 83) pitch = 48;
+//    public static void increasePitch(){
+//        pitch++;
+//        if (pitch > 83) pitch = 48;
+//    }
+//
+//    public static void decreasePitch(){
+//        pitch -=1;
+//        if (pitch < 48) pitch = 83;
+//    }
+
+    public static void setPitch(int pitchIn){
+        if (pitchIn > 83) pitchIn = 48;
+        if (pitchIn < 48) pitchIn = 83;
+        pitch = pitchIn;
     }
 
-    public static void decreasePitch(){
-        pitch -=1;
-        if (pitch < 48) pitch = 83;
-    }
 
+    public static void setVolume(int volumeIn){
+        if(volumeIn > 100) volumeIn = 100;
+        if(volumeIn < 0) volumeIn = 0;
+        volume = volumeIn;
+    }
 
     public static Integer getPitch(){
         return pitch;
     }
 
-    public static void increaseVolume(){
-        volume++;
-        if (volume > 100) volume = 100;
-    }
-
-    public static void decreaseVolume(){
-        volume -=1;
-        if (volume < 0) volume = 0;
-    }
-
     public static Integer getVolume(){
         return volume;
     }
+
+
+//    public static void increaseVolume(){
+//        volume++;
+//        if (volume > 100) volume = 100;
+//    }
+//
+//    public static void decreaseVolume(){
+//        volume -=1;
+//        if (volume < 0) volume = 0;
+//    }
 
     public static void toggleSound(){
         isSound = !isSound;
@@ -66,22 +79,19 @@ class InstrumentPlayer{
             synthesizer.open();
             MidiChannel[] channels = synthesizer.getChannels();
             instruments = synthesizer.getDefaultSoundbank().getInstruments();
-
             channels[0].programChange(instruments[50].getPatch().getProgram());
 
             while(isSound){
                 //Audio out test Parameters
                 int NoteOut = pitch;
-                if (NoteOut > 11) NoteOut = 0;
+                while (NoteOut > 11) NoteOut = NoteOut - 12;
 
                 channels[0].noteOn((pitch), (volume));
                 System.out.println("Volume: " + volume);
                 System.out.println("Pitch: " + pitch);
                 System.out.println(NoteBank[NoteOut]);
                 Thread.sleep(1000);
-
                 channels[0].allNotesOff();
-                increasePitch();
 
                 if (pitch == 0 || volume == 0)
                 {
@@ -95,7 +105,4 @@ class InstrumentPlayer{
             e.printStackTrace();
         }
     }
-
-
-
 }
